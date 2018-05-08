e37ac617695f6f2ca695fa5ac55306265ed97e7c
ritikakh <ritikakh@usc.edu>
2018-05-08 14:00:30 -0700

    HOC implemented with context routing

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 export default function(ComposedComponent) {
     class Authentication extends Component {
+
+        static contextTypes = {
+            router: PropTypes.object
+        }
+
+        componentWillMount() {
+            if (!this.props.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
+        componentWillUpdate(nextProps) {
+            if(!nextProps.authenticated) {
+                this.context.router.push('/');
+            }
+        }
+
         render() {
             return <ComposedComponent {...this.props} />
         }
     }
 
-    return Authentication;
+    function mapStateToProps(state) {
+        return { authenticated: state.authenticated };
+    }
+
+    //return Authentication;
+    return connect(mapStateToProps)(Authentication);
 }
+
+
+// Above is the skeleton of any higher order function
+
+
+/*
+//Way the HOC is used
+In a separate file, where you want to use the HOC,
+Below is the code, we will write:
+
+import Authentication // This is the HO
+import Resources // The component whihc is to be wrapped
+const ComponsedComponent = Auhentication(Resources);
+
+//and in some render method:
+
+<ComposedComponent resources={resourceList} />
+
+If in the HOC file, I do console.log(this.props.resources), it will return
+me with th resourceList that we have passsed in the render function.
+ */
