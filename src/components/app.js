beb36cfb59f8c2680f2134a946cfefbaffd249a7
ritikakh <ritikakh@usc.edu>
2018-05-08 12:42:12 -0700

    Basic app and sign in toggle button before the HOC

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Header from './header';
+import PropTypes from 'prop-types';
+
+// Component.propTypes = {
+//   text: PropTypes.string.isRequired,
+// }
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Header />
+          {this.props.children}
+      </div>
     );
   }
 }
